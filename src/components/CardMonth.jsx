0a8a4237f178e39849bd55f739e9bb2b07932bcd
Caio de Souza <caiodesouza.cds@gmail.com>
2026-04-20 09:04:51 -0300

    criado cards receitas e despesas mensal

--- src/components/CardMonth.jsx
+++ src/components/CardMonth.jsx
@@ -14,19 +14,38 @@ function CardMonth({ variant, value }) {
               <p>Mensal</p>
             </div>
           </div>
-          <div>
-            <p>Total receitas</p>
-            <h2>
-              R$
-              {value.toLocaleString("pt-BR", {
+          <div className="flex p-2 flex-col">
+            <p className="text-[#64748B] text-ms">Total receitas</p>
+            <h2 className="text-[#047756] font-semibold text-3xl">
+              {`R$ 
+              ${value.toLocaleString("pt-BR", {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
-              })}
+              })}`}
             </h2>
           </div>
         </div>
       ) : (
-        <p>teste</p>
+        <div className="flex flex-col w-full justify-between">
+          <div className="flex justify-between items-start w-full text-[#B61722] font-semibold p-2">
+            <div className="bg-[#F8E7E8] rounded-lg p-2">
+              <ArrowUp size={32} />
+            </div>
+            <div className="bg-[#F8E7E8] rounded-lg p-2 pt-1 pb-1 text-sm">
+              <p>Mensal</p>
+            </div>
+          </div>
+          <div className="flex p-2 flex-col">
+            <p className="text-[#64748B] text-ms">Total despesas</p>
+            <h2 className="text-[#B61722] font-semibold text-3xl">
+              {`-R$
+              ${value.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}`}
+            </h2>
+          </div>
+        </div>
       )}
     </div>
   );
